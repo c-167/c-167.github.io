21015f5f8a36dc6f473d46e6d956c48e013cb298
c-167 <1811437857@qq.com>
2024-11-16 12:17:10 +0800

    Site updated: 2024-11-16 12:17:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/16/hello-world/","2024/11/16/01_Java概述/"];function toRandomPost(){
+var posts=["2024/11/16/01_Java概述/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
