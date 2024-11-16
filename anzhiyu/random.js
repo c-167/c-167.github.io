ed0a421ecce1647ab1d2b5e5e2e318000fc60310
c-167 <1811437857@qq.com>
2024-11-16 13:21:36 +0800

    Site updated: 2024-11-16 13:21:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/16/01_Java概述/"];function toRandomPost(){
+var posts=["2024/11/16/01_Java概述/","2024/11/16/奖学金考试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
